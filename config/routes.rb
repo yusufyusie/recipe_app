8271078b9441a4f9faa385bc2edfff1efc3678a5
yusufyusie <yesuf023@gmail.com>
2024-01-30 23:58:47 +0300

    refacror routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  # Devise routes
+  devise_for :users
+
+  # Static pages
   get 'home', to: 'pages#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
   root "pages#index"
 
-  resources :foods
-  resources :recipes
+  # Health check
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Resource routes
+  resources :foods, only: [:index, :show]
+  resources :recipes, only: [:index, :show, :create, :update, :destroy]
 end
