165715b2870259641870730fdb3d7e18e0c48d0e
yusufyusie <yesuf023@gmail.com>
2024-01-30 17:40:59 +0300

    Create a HomeControlle 'rails generate controller Home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   get 'home', to: 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
