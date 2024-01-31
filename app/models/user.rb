25a3cb26c2a3adbfe02015beab95299d9a9013fe
yusufyusie <yesuf023@gmail.com>
2024-01-31 17:49:15 +0300

    Add validation for recipe and uder

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-         attr_accessible :name
+
+  has_many :recipes, dependent: :delete_all
+
+  validates :name, presence: true
 end
