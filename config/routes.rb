908a9db4e2f78019f2af0d79b8e56fa6e7cd3a6c
yusufyusie <yesuf023@gmail.com>
2024-01-31 05:44:55 +0300

    add public recipe route and view

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,15 @@ Rails.application.routes.draw do
   get 'home', to: 'pages#index'
   root "pages#index"
 
-  # dynamic pages
-  get 'recipes/new', to: 'recipes#new', as: 'new_recipe'
-  post 'recipes', to: 'recipes#create'
-
   # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Resource routes
   resources :foods, only: [:index, :show]
   resources :recipes, only: [:index, :show, :create, :update, :destroy]
+
+    # dynamic pages
+    get 'recipes/new', to: 'recipes#new', as: 'new_recipe'
+    post 'recipes', to: 'recipes#create'
+    get '/public_reciipes', to: 'reciipes#public_reciipes', as: 'public_reciipes'
 end
