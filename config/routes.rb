a3f3c8481ab5e2e902ce5f0037c468f87cc78e26
yusufyusie <yesuf023@gmail.com>
2024-01-31 07:24:51 +0300

    Add route for logout to redrect login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Devise routes
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+ end
 
   # Static pages
   get 'home', to: 'pages#index'
