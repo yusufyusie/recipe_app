718e24b992b134483f20bb57458c0805bce337f9
yusufyusie <yesuf023@gmail.com>
2024-01-31 17:38:30 +0300

    Add N+1 to recipe and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  :name
+         attr_accessible :name
 end
