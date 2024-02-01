b3ff0bd4e2bffe26104127c7be4a115f002197ce
yusufyusie <yesuf023@gmail.com>
2024-02-01 05:31:51 +0300

    Add setup for unit or model testing

--- Gemfile
+++ Gemfile
@@ -76,4 +76,5 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem 'capybara'
   gem 'selenium-webdriver'
+  gem 'shoulda-matchers', '~> 4.0'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_length_of(:password).is_at_least(6) }
+
+  it { should have_many(:recipes) }
+
+  describe '#name' do
+    it 'returns the correct name' do
+      expect(subject.name).to eq('Expected Name')
+    end
+  end
 end
