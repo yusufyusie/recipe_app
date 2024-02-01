c689a6750484b4eeee5f760315ae2e26c6440963
yusufyusie <yesuf023@gmail.com>
2024-02-01 13:21:45 +0300

    Replace recipes_path with user_recipes_path(current_user) in the views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,7 +6,6 @@ class RecipesController < ApplicationController
   end
 
   def show
-    # @recipe = Recipe.find(params[:id])
     @recipe = current_user.recipes.find(params[:id])
   end
 
@@ -21,7 +20,7 @@ class RecipesController < ApplicationController
   def create
     @recipe = current_user.recipes.build(recipe_params)
     if @recipe.save
-      redirect_to user_recipes_path(current_user, @recipes), notice: 'Recipe was successfully created.'
+      redirect_to user_recipes_path(current_user), notice: 'Recipe was successfully created.'
     else
       render :new
     end
@@ -31,7 +30,7 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
 
     if @recipe.update(recipe_params)
-      redirect_to user_recipes_path(current_user, @recipes), notice: 'Recipe was successfully updated.'
+      redirect_to user_recipes_path(current_user), notice: 'Recipe was successfully updated.'
     else
       render :edit
     end
