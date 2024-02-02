be29e3eadc079959370d58091a1a143367fbc413
yusufyusie <yesuf023@gmail.com>
2024-02-02 21:12:22 +0300

    Update to ShoppingListsController add function

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -6,7 +6,7 @@ class ShoppingListsController < ApplicationController
     @ingredients = RecipeFood.where(recipe: @recipes).group_by { |ingredient| ingredient.food.name }
 
     @total_price = @ingredients.map do |food_name, ingredients|
-      total_quantity_required = ingredients.sum(&:quantity)
+      total_quantity_required = ingredients.map(&:quantity).compact.sum # Add compact before sum
       user_food = @user.foods.find_by(name: food_name)
       missing_quantity = user_food ? [total_quantity_required - user_food.quantity.to_i, 0].max : total_quantity_required
       @missing_numbers[food_name] = missing_quantity
