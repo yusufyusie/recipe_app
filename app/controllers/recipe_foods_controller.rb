78d83b079056eed11597a6622762dc2807a51c9c
yusufyusie <yesuf023@gmail.com>
2024-02-02 20:13:10 +0300

    Update the files to add recipe food part

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,23 +1,26 @@
-# app/controllers/recipe_foods_controller.rb
-
 class RecipeFoodsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user
-  before_action :set_recipe_food, only: %i[show edit update destroy]
+  before_action :set_recipe, only: [:create]
+  before_action :set_recipe_food, only: [:edit, :update, :destroy, :show]
 
   def index
     @recipe_foods = @user.recipe_foods.includes(:recipe, :food)
   end
 
   def new
-    @recipe_food = @user.recipe_foods.build
+    @user = User.find(params[:user_id])
+    @recipe = @user.recipes.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.new
   end
 
   def create
-    @recipe_food = @user.recipe_foods.build(recipe_food_params)
+    @recipe_food = @recipe.recipe_foods.build(recipe_food_params)
     if @recipe_food.save
-      redirect_to recipe_foods_path, notice: 'Recipe food added successfully.'
+      flash[:success] = 'Ingredient added successfully!'
+      redirect_to recipe_path(@recipe)
     else
+      flash.now[:error] = 'Error: ingredient could not be added!'
       render :new
     end
   end
@@ -28,15 +31,18 @@ class RecipeFoodsController < ApplicationController
 
   def update
     if @recipe_food.update(recipe_food_params)
-      redirect_to recipe_foods_path, notice: 'Recipe food updated successfully.'
+      flash[:notice] = 'Ingredient updated successfully!'
+      redirect_to recipe_path(@recipe_food.recipe_id)
     else
+      flash.now[:error] = 'Error: ingredient could not be updated!'
       render :edit
     end
   end
 
   def destroy
-    @recipe_food.destroy
-    redirect_to recipe_foods_path, notice: 'Recipe food deleted successfully.'
+    @recipe_food.destroy!
+    flash[:success] = 'Ingredient was deleted successfully!'
+    redirect_to recipe_path(@recipe_food.recipe_id)
   end
 
   private
@@ -45,11 +51,15 @@ class RecipeFoodsController < ApplicationController
     @user = current_user
   end
 
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
   def set_recipe_food
     @recipe_food = @user.recipe_foods.find(params[:id])
   end
 
   def recipe_food_params
-    params.require(:recipe_food).permit(:quantity, :recipe_id, :food_id)
+    params.require(:recipe_food).permit(:quantity, :food_id)
   end
 end
